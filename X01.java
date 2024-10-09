9f37eba9d802e160a2953de6cc7a43c5ac3ace37
Choqy Pananda Sirait <krisky325@gmail.com>
2024-10-09 16:35:59 +0700

    Pair-11

--- X01.java
+++ X01.java
@@ -1,2 +1,39 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24006 - Margareth Bungaran Sitompul
+// 12S24012 - Choqy Pananda Sirait
+import java.util.*;
+import java.lang.Math;
+
+public class X01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double palingmurah, harga, total;
+
+        total = 0;
+        palingmurah = 999;
+        harga = Double.parseDouble(input.nextLine());
+        while (harga != 0) {
+            if (harga == 0) {
+            } else {
+                if (harga < palingmurah) {
+                    palingmurah = harga;
+                } else {
+                    harga = harga;
+                }
+                total = total + harga;
+            }
+            harga = Double.parseDouble(input.nextLine());
+        }
+        if (total >= 100) {
+            total = total - palingmurah;
+        } else {
+            total = total;
+        }
+        System.out.println(toFixed(total,2));
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
+
